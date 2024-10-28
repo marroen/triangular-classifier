12a6cbf15bb9aae7b00b70b434d976b7e5849d74
Martin Ronning <ronningmartin@outlook.com>
2024-10-28 17:39:16 +0100

    add flag -n, determine number of vertices for plot

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
+import random
+import argparse
 
-x = [4, 5, 10, 4, 3, 11, 14 , 8, 10, 12]
-y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
-classes = [0, 0, 1, 0, 0, 1, 1, 0, 1, 1]
+def main():
+  parser = argparse.ArgumentParser(description="Triangular Classifier.")
+  parser.add_argument('-n', dest="n", required=False, type=int, help="The number of vertices.")
+  args = parser.parse_args()
+  n = args.n
 
-plt.scatter(x, y, c=classes)
-plt.show() 
+  x = []
+  y = []
+  classes = []
+
+  for _ in range(0, n):
+    x.append(random.randint(0, 8) + random.random())
+    y.append(random.randint(0, 8) + random.random())
+    classes.append(random.randint(0,1))
+
+  plt.scatter(x, y, c=classes)
+  plt.show() 
+
+if __name__ == "__main__":
+    main()
