86a8094ebf75ba1ec7dd45f0e28fd052d5f486bd
hungrysnake3022 <hungrysnake3022@gmail.com>
2024-10-30 01:58:15 +0100

    Finished data generation for 1 and 2
    Finished the training data. The data is now labeled 0 if i's outside of the triangle and 1 if it's inside of the the triangle. I also already added the noise parameter and let the data depend on that. Only thing still needed for the data generation is the editing of the triangle, but that should not be too difficult and i'll probably do that tomorrow (today? :eyes: )
    
    TODO: make the data generation for the different sized triangles, make the knn model and test, export the amount of misclassifications to excel

--- main.py
+++ main.py
@@ -6,18 +6,31 @@ import argparse
 def main():
   parser = argparse.ArgumentParser(description="Triangular Classifier.")
   parser.add_argument('-n', dest="n", required=True, type=int, help="The number of vertices.")
+  parser.add_argument('-f', dest="f", required=True, type=float, help="Noise in the data.")
+
 
   args = parser.parse_args()
   n = args.n
+  f = args.f
 
   x = []
   y = []
   classes = []
 
   for _ in range(0, n):
-    x.append(random.randint(0, 8) + random.random())
-    y.append(random.randint(0, 8) + random.random())
-    classes.append(random.randint(0,1))
+    x.append(random.randint(0, 9) + random.random())
+    y.append(random.randint(0, 9) + random.random())
+    label = 1
+    if(x[-1]>7):
+        label = 0
+    if(y[-1]<3):
+        label = 0
+    if(y[-1]>x[-1]):
+        label = 0
+        
+    if(random.randint(0, 99) < f*100):
+        label = 1 - label
+    classes.append(label)
 
   plt.scatter(x, y, c=classes)
   plt.title(f"n = {n}")
